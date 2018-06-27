4ac2117a3443f5fdb8ff1cc6241431c110297df8
leonsuetfeld <mail@leonsuetfeld.com>
2018-06-27 17:31:57 +0200

    folder structure

--- 2_sched/m_advanced_scheduler_create_csv.py
+++
@@ -1,6 +0,0 @@
-import m_advanced_scheduler as asc
-
-if __name__ == '__main__':
-
-    scheduling_subfolder, experiment_path, experiment_name, spec_list, n_runs, gridjob_command = asc.get_settings()
-    asc.csv_update(scheduling_subfolder, experiment_name+'.csv', experiment_path, experiment_name, spec_list, n_runs, mark_running_as_incomplete=False) # set this to False when executing during gridjob
